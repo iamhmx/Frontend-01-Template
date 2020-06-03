e4194f62d8c59f5ec7df12b2deb34dbddf4868d5
helloyellow <helloyellow@aliyun.com>
2020-06-03 20:54:29 +0800

    week08

---
+++ week08/match.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: mingxing.huang
+ * @Date: 2020-06-03 20:53:54
+ */
+function match(element, selector) {
+    if (!selector || !element.attributes) {
+        return false;
+    }
+    if (selector.charAt(0) == '#') {
+        var attr = element.attributes.filter(attr => attr.name == 'id')[0];
+        if (attr && attr.value === selector.replace("#", '')) {
+            return true;
+        }
+    } else if (selector.charAt(0) == ".") {
+        var attr = element.attributes.filter(attr => attr.name === "class")[0]
+        if (attr && attr.value === selector.replace(".", ''))
+            return true;
+    } else {
+        if (element.tagName === selector) {
+            return true;
+        }
+    }
+}
